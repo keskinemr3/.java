7ee1442e83deb5c268a7015ad238eb7b2230abc2
Emre Keskin <keskin.e0130@gmail.com>
2026-08-23 19:19:02 +0300

    Add files via upload

---
+++ untitled2/classes/src/CustomerManager.java
@@ -0,0 +1,23 @@
+
+// Class
+// es
+// Classes
+// sadece sout var şimdilik ama düzelir ya düzelir
+
+public class CustomerManager {
+
+    public void Add(){
+        System.out.println("Müşteri Eklendi");
+
+    }
+
+    public void Remove(){
+        System.out.println("Müşteri silindi");
+
+    }
+
+    public void Update(){
+        System.out.println("Müşteri Güncellendi");
+
+    }
+}
